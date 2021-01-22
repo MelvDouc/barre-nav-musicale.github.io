b2e2bd0a8bb79f70cea2350272b1576aea125424
MelvDouc <melv.douc@gmail.com>
2021-01-22 08:07:44 +0100

    résolu pb boucle

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const liensNav = document.querySelectorAll('nav ul > li');
 const fichiersAudio = document.getElementsByTagName('audio');
 const arcEnCiel = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-// [1] Couleurs des touches
-
+// [1] Événements liens navbar
 for(let i = 0; i < liensNav.length; i++){
+    // [1-a] Couleurs des touches
     liensNav[i].addEventListener('mouseover', function() {
         this.style.backgroundColor = arcEnCiel[i];
         // Parce que "mi" en blanc se voit mal sur fond jaune :
@@ -18,12 +18,17 @@ for(let i = 0; i < liensNav.length; i++){
         this.style.backgroundColor = 'initial';
         this.firstElementChild.style.webkitTextFillColor = 'transparent'
     });
+    // [1-b] Lecture audio au survol (ne pas placer avant [1-a])
+    liensNav[i].addEventListener('mousemove', () => {
+        fichiersAudio[i].play();
+    })
 };
 
-// [2] Lecture audio au survol (ne pas placer avant [1])
-
-for(let i = 0; liensNav; i++){ /* Ne fonctionne pas avec ;i < liensNav; */
+/*
+// Bizarrement, cette boucle fonctionne aussi avec la simple condition "liensNav" mais seulement pour cet événement.
+for(let i = 0; liensNav; i++){
     liensNav[i].addEventListener('mousemove', () => {
         fichiersAudio[i].play();
     })
 }
+*/
